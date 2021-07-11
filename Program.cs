1ae2a99555c822d51ba553b58dc7f5117cbfe4f9
Amanda <amanda.garrison@improving.com>
2021-07-11 16:38:26 -0500

    completed step 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace CalculatorApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1;
+            int num2;
+
+            Console.WriteLine("A Consonle Calculator");
+           
+            Console.WriteLine("Enter a number.");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter a second number and I will add it to the first");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Result: " + (num1 + num2));
         }
     }
 }
